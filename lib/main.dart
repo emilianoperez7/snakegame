8c8a24891de8bb979c9c5fae34171bd4bee9f4f6
Emiliano Pérez <emilianojesusp599@gmail.com>
2025-02-19 19:08:26 -0600

    Added Comments

--- lib/main.dart
+++ lib/main.dart
@@ -21,41 +21,56 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //Posición de la serpiente en la tabla en forma de lista
   static List<int> snakePosition = [45, 65, 85, 105, 125];
+  //Número de cuadros en la tabla del juego
   int numberOfSquares = 760;
+
   static var randomNumber = Random();
+  //Posicion de la comida en la tabla
   int food = randomNumber.nextInt(700);
 
+  //Funcion para generar la comida en una posicion aleatoria
   void generateNewFood() {
     food = randomNumber.nextInt(700);
+    //While que asegura que la comida no aparezca en la posicion de la serpiente
     while (snakePosition.contains(food)) {
       food = randomNumber.nextInt(700);
     }
   }
 
+  //Funcion para iniciar el juego
   void startGame() {
+    //Posicion incial de la serpiente
     snakePosition = [45, 65, 85, 105, 125];
-    const duration = const Duration(milliseconds: 100);
+
+    //Temporizador de duracion 100 ms para mover la serpiente
+    const duration = Duration(milliseconds: 100);
     Timer.periodic(duration, (Timer timer) {
       updateSnake();
       if (gameOver()) {
         timer.cancel();
+        //En caso de perder, el temporizador se detiene y muestra la pantalla de game over
         _showGameOverScreen();
       }
     });
   }
 
+//Funcion para mover la serpiente en la direccion que se desee y de "teletransportarla" al pasar de los bordes de la pantalla
   var direction = 'down';
   void updateSnake() {
     setState(() {
       switch (direction) {
+        //Se mueve hacia abajo
         case 'down':
           if (snakePosition.last > 740) {
             snakePosition.add(snakePosition.last + 20 - 760);
           } else {
             snakePosition.add(snakePosition.last + 20);
           }
+
           break;
+        //Se mueve hacia arriba
         case 'up':
           if (snakePosition.last < 20) {
             snakePosition.add(snakePosition.last - 20 + 760);
@@ -63,6 +78,7 @@ class _HomePageState extends State<HomePage> {
             snakePosition.add(snakePosition.last - 20);
           }
           break;
+        //Se mueve hacia la izquierda
         case 'left':
           if (snakePosition.last % 20 == 0) {
             snakePosition.add(snakePosition.last - 1 + 20);
@@ -70,6 +86,7 @@ class _HomePageState extends State<HomePage> {
             snakePosition.add(snakePosition.last - 1);
           }
           break;
+        //Se mueve hacia la derecha
         case 'right':
           if ((snakePosition.last + 1) % 20 == 0) {
             snakePosition.add(snakePosition.last + 1 - 20);
@@ -77,8 +94,10 @@ class _HomePageState extends State<HomePage> {
             snakePosition.add(snakePosition.last + 1);
           }
           break;
+
         default:
       }
+      // Si la serpiente come la comida, se genera una nueva, de lo contrario, se elimina la ultima posición de la serpiente
       if (snakePosition.last == food) {
         generateNewFood();
       } else {
@@ -87,6 +106,7 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  //Se verifica si la serpiente se come a si misma, y en ese caso se detiene el juego
   bool gameOver() {
     for (int i = 0; i < snakePosition.length; i++) {
       int count = 0;
@@ -102,6 +122,7 @@ class _HomePageState extends State<HomePage> {
     return false;
   }
 
+  //Pantalla de game over, muestra cuantas manzanas se comieron y un boton para volver a jugar
   void _showGameOverScreen() {
     showDialog(
         context: context,
@@ -134,11 +155,13 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //appBar: appBar(),
       backgroundColor: Colors.black,
       body: Column(
         children: <Widget>[
           Expanded(
             child: GestureDetector(
+              // Detecta el gesto tactil vertical para cambiar la dirección de la serpiente
               onVerticalDragUpdate: (details) {
                 if (direction != 'up' && details.delta.dy > 0) {
                   direction = 'down';
@@ -146,6 +169,7 @@ class _HomePageState extends State<HomePage> {
                   direction = 'up';
                 }
               },
+              // Detecta el gesto tactil horizontal para cambiar la dirección de la serpiente
               onHorizontalDragUpdate: (details) {
                 if (direction != 'left' && details.delta.dx > 0) {
                   direction = 'right';
@@ -153,6 +177,7 @@ class _HomePageState extends State<HomePage> {
                   direction = 'left';
                 }
               },
+
               child: Container(
                 child: GridView.builder(
                     physics: NeverScrollableScrollPhysics(),
@@ -160,6 +185,7 @@ class _HomePageState extends State<HomePage> {
                     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                         crossAxisCount: 20),
                     itemBuilder: (BuildContext context, int index) {
+                      //Se utiliza verde para cada cuadro en la tabla que representa la serpiente, de rojo la comida y de gris los cuadros vacios
                       if (snakePosition.contains(index)) {
                         return Center(
                           child: Container(
@@ -168,10 +194,10 @@ class _HomePageState extends State<HomePage> {
                               borderRadius: BorderRadius.circular(5),
                               child: Container(
                                 color: const Color.fromARGB(255, 0, 255, 26),
-                              ),
-                            ),
-                          ),
-                        );
+                              ), //Container
+                            ), //ClipRRect
+                          ), //Container
+                        ); //Center
                       }
                       if (index == food) {
                         return Container(
@@ -179,20 +205,23 @@ class _HomePageState extends State<HomePage> {
                           child: ClipRRect(
                               borderRadius: BorderRadius.circular(5),
                               child: Container(
-                                  color: const Color.fromARGB(255, 230, 0, 0))),
+                                  color: const Color.fromARGB(
+                                      255, 230, 0, 0))), //ClipRRect
                         );
                       } else {
                         return Container(
                           padding: EdgeInsets.all(2),
                           child: ClipRRect(
                               borderRadius: BorderRadius.circular(5),
-                              child: Container(color: Colors.grey[900])),
-                        );
+                              child: Container(
+                                  color: Colors.grey[900])), //ClipRRect
+                        ); //Container
                       }
-                    }),
-              ),
-            ),
-          ),
+                    }), //GridView.builder
+              ), //Container
+            ), //GestureDetector
+          ), //Expanded
+          //Franja en la parte inferior de la pantalla que muestra el boton de start game y el nombre del juego
           Padding(
             padding:
                 const EdgeInsets.only(bottom: 20.0, left: 20.0, right: 20.0),
@@ -206,19 +235,19 @@ class _HomePageState extends State<HomePage> {
                     style: TextStyle(
                         color: const Color.fromARGB(255, 0, 238, 255),
                         fontSize: 20),
-                  ),
-                ),
+                  ), //Text
+                ), //GestureDetector
                 Text(
                   'SNAKE GAME',
                   style: TextStyle(
                       color: const Color.fromARGB(255, 21, 0, 255),
                       fontSize: 20),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+                ) //Text
+              ], //<Widget>[]
+            ), //Row
+          ) //Padding
+        ], //<Widget>[]
+      ), //Column
+    ); //Scaffold
   }
 }
